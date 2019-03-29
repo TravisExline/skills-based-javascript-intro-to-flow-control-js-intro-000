9e67105333d00f78065cbbda15935ead09df16a7
Travis Exline <t.exline22@gmail.com>
2019-03-29 14:10:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,21 +1,21 @@
 function basicTeenager(age) {
-if (age === 13 && 19) {return "You are a teenager!"}
+if (age >= 13 && <= 19) {return "You are a teenager!"}
 }
 
 function teenager(age) {
-if (age === 13 && 19) {return "You are a teenager!"}
+if (age >= 13 && <= 19) {return "You are a teenager!"}
 else if (age <= 12) {return "You are not a teenager"}
 else {return "You are not a teenager"}
 }
 
 function ageChecker(age) {
-if (age === 13 && 19) {return "You are a teenager!"}
+if (age >= 13 && <= 19) {return "You are a teenager!"}
 else if (age <= 12) {return "You are a kid"}
 else {return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
-console.log('You' + (age >= 13 <= 19 ? 'You are a teenager' : 'You are not a teenager'));
+return age 
 }
 
 function switchAge(age) {
