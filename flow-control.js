58619e713ab8905a80ca31026befd722277e68a7
Travis Exline <t.exline22@gmail.com>
2019-03-29 13:45:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,6 +5,7 @@ if (age === 13 && 19) {return "You are a teenager!"}
 function teenager(age) {
 if (age === 13 && 19) {return "You are a teenager!"}
 else if (age <= 12) {return "You are not a teenager"}
+else {return "You are not a teenager"}
 }
 
 function ageChecker(age) {
