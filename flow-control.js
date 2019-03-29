9bc20763e450e6f2987ed5d08c7e50b30b40af2d
Travis Exline <t.exline22@gmail.com>
2019-03-29 14:25:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ else {return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
-return conditionToTest(age >=13 && age <=19) ?
+  return (age >=13 && age <=19) ?
 "You are a teenager" : "You are not a teeneger"
 }
 
